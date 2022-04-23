666f9b12a24fab035148bbe62ba410c1a7327275
FoundedNahte <56288341+FoundedNahte@users.noreply.github.com>
2022-04-23 11:41:04 -0400

    change

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,4 +1,5 @@
 use actix_web::{web, HttpResponse};
+use sqlx::PgConnection;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -6,6 +7,23 @@ pub struct FormData {
     name: String
 }
 
-pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+pub async fn subscribe(
+    _form: web::Form<FormData>,
+    _connection: web::Data<PgConnection>,
+) -> HttpResponse {
+    sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    // We use 'get_ref' to et an immutable reference to the 'PgConnetion'
+    // wrapped by 'web::Data'
+    .execute(connection.get_ref())
+    .await;
     HttpResponse::Ok().finish()
 }
