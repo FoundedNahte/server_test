2f6e035d2a051d0e75b3b21a43d1caa6c92872b9
FoundedNahte <56288341+FoundedNahte@users.noreply.github.com>
2022-04-23 02:16:45 -0400

    4/23/2022 change

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use servertest::run;
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run("127.0.0.1:8000")?.await
+    let listener = TcpListener::bind("127.0.0.1:8000")
+        .expect("Failed to bind port 8000");
+    run(listener)?.await
 }
