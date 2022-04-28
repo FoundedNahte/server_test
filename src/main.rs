7db1e759952b8e29d6fe87200007b96d57c78456
FoundedNahte <eclau2003@gmail.com>
2022-04-28 00:53:40 -0400

    4/28/2022:1253am

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use servertest::configuration::get_configuration;
 use servertest::startup::run;
 use servertest::telemetry::{get_subscriber, init_subscriber};
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 use secrecy::ExposeSecret;
 
@@ -11,11 +11,15 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string().expose_secret())
-        .await
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(&configuration.database.connection_string().expose_secret())
         .expect("Failed to connect to Postgres.");
     // We have removed the hard-coded '8000' - it's now coming from our settings!
-    let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await
 }
