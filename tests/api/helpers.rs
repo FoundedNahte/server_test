763b3718c7b49626f0d72955c92335a4f21e2393
FoundedNahte <eclau2003@gmail.com>
2022-05-04 14:12:38 -0400

    5/4/22:1412

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -6,6 +6,7 @@ use servertest::configuration::{get_configuration, DatabaseSettings};
 use servertest::email_client::EmailClient;
 use servertest::startup::{run, Application, get_connection_pool};
 use servertest::telemetry::{get_subscriber, init_subscriber};
+use wiremock::MockServer;
 
 
 // Ensure that the 'tracing' stack is only initialised only once using 'once_cell'
@@ -25,10 +26,25 @@ static TRACING: Lazy<()> = Lazy::new(|| {
 pub struct TestApp {
     pub address: String,
     pub db_pool: PgPool,
+    pub email_server: MockServer,
+}
+
+impl TestApp {
+    pub async fn post_subscriptions(&self, body: String) -> reqwest::Response {
+        reqwest::Client::new()
+            .post(&format!("{}/subscriptions", &self.address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("FAiled to execute request.")
+    }
 }
 
 pub async fn spawn_app() -> TestApp {
     Lazy::force(&TRACING);
+
+    let email_server = MockServer::start().await;
 
     let configuration = {
         let mut c = get_configuration().expect("Failed to read configuration.");
@@ -48,43 +64,10 @@ pub async fn spawn_app() -> TestApp {
     TestApp {
         address,
         db_pool: get_connection_pool(&configuration.database),
+        email_server,
     }
 }
-/*
-pub async fn spawn_app() -> TestApp {
-    Lazy::force(&TRACING);
 
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-    // We retrieve the port assigned to us by the OS
-    let port = listener.local_addr().unwrap().port();
-    let address = format!("http://127.0.0.1:{}", port);
-
-    let mut configuration = get_configuration().expect("Failed to read configuration.");
-    configuration.database.database_name = Uuid::new_v4().to_string();
-    let connection_pool = configure_database(&configuration.database).await;
-
-    // Build a new email client
-    let sender_email = configuration
-        .email_client
-        .sender()
-        .expect("Invalid sender email address.");
-    let timeout = configuration.email_client.timeout();
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-        timeout
-    );
-
-    let server =
-        run(listener, connection_pool.clone(), email_client).expect("Failed to bind address");
-    let _ = tokio::spawn(server);
-    TestApp {
-        address,
-        db_pool: connection_pool,
-    }
-}
-*/
 pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
     // Create Database
     let mut connection = PgConnection::connect_with(&config.without_db())
